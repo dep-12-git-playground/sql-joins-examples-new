944815e400f21cb1d445dae89bf8293175e11717
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-08-06 19:29:22 +0530

    Add student records

--- db-script.sql
+++ db-script.sql
@@ -2,3 +2,14 @@ CREATE TABLE student(
     id VARCHAR(10) PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
+
+INSERT INTO student (id, name) VALUES ('S001', 'Kasun');
+INSERT INTO student (id, name) VALUES ('S002', 'Nuwan');
+INSERT INTO student (id, name) VALUES ('S003', 'Sampath');
+INSERT INTO student (id, name) VALUES ('S004', 'Chamika');
+INSERT INTO student (id, name) VALUES ('S005', 'Asiri');
+INSERT INTO student (id, name) VALUES ('S006', 'Menith');
+INSERT INTO student (id, name) VALUES ('S007', 'Prajeeth');
+INSERT INTO student (id, name) VALUES ('S008', 'Sachith');
+INSERT INTO student (id, name) VALUES ('S009', 'Dulanga');
+INSERT INTO student (id, name) VALUES ('S010', 'Ravindya');
